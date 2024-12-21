7a3dfa833ee8843cbdf1ca41107dae91ac25d9df
Samadsust71 <samadsust71@gmail.com>
2024-12-21 23:32:56 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h1 className='bg-slate-700 text-red-500'>Vite + React</h1>
+      <button className="btn btn-success">Hello</button>
+     
+     
+    </>
+  )
+}
+
+export default App
