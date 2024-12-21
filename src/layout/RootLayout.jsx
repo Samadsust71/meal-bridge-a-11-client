0f6de279cb5cc461113bb54a3b792bb89734857a
Samadsust71 <samadsust71@gmail.com>
2024-12-22 01:03:12 +0600

    added react router

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,12 @@
+
+
+const RootLayout = () => {
+  return (
+    <div>
+      <h1 className="text-2xl bg-green-400 text-white">Root layout </h1>
+      <button className="btn btn-primary">Hello from root</button>
+    </div>
+  )
+}
+
+export default RootLayout
