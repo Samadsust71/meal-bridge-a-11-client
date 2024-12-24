d9dd1d2247e59c7ce0eae75c0de5be48d94aa57d
Samadsust71 <samadsust71@gmail.com>
2024-12-25 04:03:40 +0600

    added banner image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,10 @@ import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import slideImage1 from "../assets/banner-image/slide-1.jpg"
+import slideImage2 from "../assets/banner-image/slide-2.webp"
+import slideImage3 from "../assets/banner-image/slide-3.jpg"
+import { Link } from "react-router-dom";
 
 
 const Banner = () => {
@@ -12,19 +16,19 @@ const Banner = () => {
       subtitle:
         "Together, we can turn surplus into smiles. Join the movement today!",
       
-      image: "https://muslimi.com/wp-content/uploads/2023/04/da.jpg",
+      image: slideImage1,
     },
     {
       title: "Every Bite Matters, for Revolution",
       subtitle:
         "Empowering communities through food sharing. Donate or request with ease.",
-      image: "https://thumbs.dreamstime.com/b/charity-food-hope-poor-who-have-no-money-concept-begging-donate-to-people-society-volunteers-share-149431320.jpg",
+      image: slideImage2,
     },
     {
       title: "From Extra to Extraordinary",
       subtitle:
         "Reduce waste, feed hope, and build stronger communities with MealBridge.",
-      image: "https://media.istockphoto.com/id/1060352206/photo/donate-food-to-hungry-people-concept-of-poverty-and-hunger.jpg?s=612x612&w=0&k=20&c=BB37IPajPeKo8fsJ3m7z2TOUmCbD0nnJo79ZXBg89sM=",
+      image: slideImage3,
     },
   ];
 
@@ -49,15 +53,15 @@ const Banner = () => {
             <img
               src={slide.image}
               alt={slide.title}
-              className="w-full h-[600px] object-cover"
+              className="w-full h-[500px] object-cover"
             />
             <div className="absolute inset-0 bg-black bg-opacity-50"></div> 
             <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-white space-y-8">
-              <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">{slide.title}</h2>
-              <p className="mt-2 lg:text-xl">{slide.subtitle}</p>
-              <button className="px-6 py-3 bg-transparent text-white font-semibold hover:bg-[#90BE6D] border border-white transition-all duration-100 ease-in-out rounded-full">
+              <h2 className="text-3xl md:text-4xl lg:text-6xl font-bold text-center">{slide.title}</h2>
+              <p className="mt-2 lg:text-xl text-center">{slide.subtitle}</p>
+              <Link to={'/add-food'} className="px-6 py-3 bg-transparent text-white font-semibold hover:bg-[#90BE6D] border border-white transition-all duration-100 ease-in-out rounded-full">
                 JOIN NOW
-              </button>
+              </Link>
             </div>
           </SwiperSlide>
         ))}
